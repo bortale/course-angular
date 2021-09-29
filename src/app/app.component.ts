557709b7af916fd3df643bb20be319c0ccb0c258
bortale <alebortna@gmail.com>
2021-09-29 20:14:37 +0200

    new editions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  name = 'ToDoList';
+
+  products = [{
+    name: 'make piece with my Bubochka',
+    time: 'as soon as possible',
+    reason: 'because he understands me better than anyone',
+    status: true
+  }, {
+    name: 'make piece with my Bubochka',
+    time: 'very fast because we just can`t fight long...it is not good thing to do together',
+    reason: 'because i need him very much',
+    status: true
+  }, {
+    name: 'make piece with my Bubochka',
+    time: 'we can`t fight long because you can`t live long without me:))))',
+    reason: 'because i am sorry i am very deep digger...sorry',
+    status: false
+  }]
+
+  isOpenMenu = false;
+
+  openDropDown() {
+    this.isOpenMenu = !this.isOpenMenu
+  }
+
 }
